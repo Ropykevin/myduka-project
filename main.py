f0bf6c5a47285e60d5d8581eaf1d83420aade741
ropykevin <ropykevin@gmail.com>
2025-10-29 20:50:57 -0900

    flask day 5

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask,render_template,request,redirect,url_for
-from database import fetch_data,insert_products
+from database import fetch_data,insert_products,insert_sales
 
 app=Flask(__name__)
 
@@ -34,8 +34,30 @@ def add_products():
 @app.route('/sales')
 def sales():
     sales=fetch_data('sales')
-    return render_template('sales.html',sales=sales)
+    # fetch products to display on select
+    products=fetch_data('products')
+    return render_template('sales.html',sales=sales,products=products)
 
+# make sale route
+
+@app.route('/make_sales',methods=['GET','POST'])
+def make_sale():
+    if request.method=='POST':
+        pid=request.form['pid']
+        qty=request.form['qty']
+        
+        new_sale=(pid,qty)
+        insert_sales(new_sale)
+        return redirect(url_for('sales'))
+    return redirect(url_for('sales')) 
+# steps to make sale
+# 1.modal with the form
+# 2. > the  form should be able to display all the products on a select option
+# 3.On the sales route fetch products to be displayed
+# 4.add fetched products to render template function
+# 5.loop the fetched products into the option element in the select element on sales.html
+# 6.on value attribute pass the product id while btw the opening and the closing option tag have the product name
+        
 # stocks route 
 
 @app.route('/stock')
